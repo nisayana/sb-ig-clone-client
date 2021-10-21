b8b405c91865216107dae7c9019e1ef992de1522
nisayana <nimaevasb@gmail.com>
2021-10-20 20:27:48 -0400

    added form to collect file data for request to backend

--- src/components/UploadPosts.js
+++ src/components/UploadPosts.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function UploadPosts() {
+
+    const [formData, setFormData] = useState({
+        file: {},
+        caption: ""
+    })
+
+    const hadnleFileChange = (e) => {
+        setFormData((prevState) => {
+            return {
+                ...prevState,
+                [e.target.name]: e.target.files[0]
+            }
+        })
+    }
+
     return(
         <div>
-            Post Upload Page
+            <form>
+                <input onLoad={hadnleFileChange} name="file" type="file"></input><br/>
+                <input name="caption" type="text" placeholder="caption"></input><br/>
+                <button type="submit">Submit</button><br/>
+            </form>
         </div>
     )
 }
