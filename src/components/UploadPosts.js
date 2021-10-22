0001950ab04c18d2bfc771c475f86b4c3e5c65d5
nisayana <nimaevasb@gmail.com>
2021-10-21 22:47:30 -0400

    upload photos

--- src/components/UploadPosts.js
+++ src/components/UploadPosts.js
@@ -7,6 +7,16 @@ export default function UploadPosts() {
         caption: ""
     })
 
+
+    const handleChange = (e) => {
+        setFormData((prevState) => {
+            return {
+                ...prevState,
+                [e.target.name]: e.target.files[0]
+            }
+        })
+    }
+
     const hadnleFileChange = (e) => {
         setFormData((prevState) => {
             return {
@@ -16,11 +26,35 @@ export default function UploadPosts() {
         })
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const {file, caption} = formData
+        let params = {
+            post: {
+                file,
+                caption
+            }
+        }
+        fetch("http://localhost:3001/posts", {
+            "method": "POST",
+            "headers": {
+               "Content-type": "Application/json",
+               "Accept": "Application/json"
+            },
+            "body": JSON.stringify(params)
+        })
+        .then(resp => resp.json())
+        .then(json => {
+            debugger
+        })
+        .catch(error => console.error(error))
+    }
+
     return(
         <div>
             <form>
-                <input onLoad={hadnleFileChange} name="file" type="file"></input><br/>
-                <input name="caption" type="text" placeholder="caption"></input><br/>
+                <input onChange={hadnleFileChange} name="file" type="file"></input><br/>
+                <input onChange={handleChange} value={formData.caption} name="caption" type="text" placeholder="caption"></input><br/>
                 <button type="submit">Submit</button><br/>
             </form>
         </div>
